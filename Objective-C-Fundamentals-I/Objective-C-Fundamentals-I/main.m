254afedce01837e9c9f2cd7ecd7466db598e2c25
audreywelch <welch.audreyl@gmail.com>
2020-05-01 14:31:48 -0700

    Project created

---
+++ Objective-C-Fundamentals-I/Objective-C-Fundamentals-I/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C-Fundamentals-I
+//
+//  Created by Audrey Welch on 5/1/20.
+//  Copyright © 2020 Audrey Welch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
